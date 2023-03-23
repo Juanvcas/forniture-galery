33444d886eaf7baf1e2b32c5234c5126835000fa
Juan Vasquez <juanvcas96@gmail.com>
2023-03-22 21:37:09 -0500

    [INIT] First commit with the initial project files.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import '../styles/pages/Home.css';
+
+function Home() {
+	return (
+		<main>
+			<section>
+				<h1>Mobiliario de oficina</h1>
+			</section>
+		</main>
+	);
+}
+
+export default Home;
